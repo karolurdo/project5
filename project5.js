262485a1f3e827f9c62f82caf2724ff46856bed8
Urdo <karol.ugarenko@gmail.com>
2022-01-29 20:41:58 +0100

    Create background color change

--- project5.js
+++ project5.js
@@ -1,5 +1,6 @@
 const h1 = document.querySelector('h1');
 document.body.addEventListener('mousemove', function(event){  //listener
-   
-   h1.textContent = `X: ${event.clientX} Y:${event.clientY}`; // position cursor
+      h1.textContent = `X: ${event.clientX} Y:${event.clientY}`; // position cursor
+
+      document.body.style.backgroundColor = `rgb(${event.clientX}, ${event.clientY}, 100)`;
 })
