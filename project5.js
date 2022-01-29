4a59a77f4c7b02e5c155133a55e811a0fc670146
Urdo <karol.ugarenko@gmail.com>
2022-01-29 20:43:07 +0100

    fix coment

--- project5.js
+++ project5.js
@@ -1,6 +1,6 @@
 const h1 = document.querySelector('h1');
 document.body.addEventListener('mousemove', function(event){  //listener
-      h1.textContent = `X: ${event.clientX} Y:${event.clientY}`; // position cursor
+      h1.textContent = `X: ${event.clientX} Y:${event.clientY}`; // show position cursor
 
-      document.body.style.backgroundColor = `rgb(${event.clientX}, ${event.clientY}, 100)`;
+      document.body.style.backgroundColor = `rgb(${event.clientX}, ${event.clientY}, 100)`; //change color background
 })
